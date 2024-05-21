36db1a1686989b3c9b44b38761d0a4ed91c7bfb1
Dennis Schiese <135758800+dschiese@users.noreply.github.com>
2024-05-21 13:01:02 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from rdflib import Graph
-
-string = """@prefix explanations: <urn:qanary:explanations#> .@prefix rdfs:         <http://www.w3.org/2000/01/rdf-schema#> .<urn:qanary:QE-SparqlQueryExecutedAutomaticallyOnWikidataOrDBpedia>        explanations:hasExplanationForCreatedData                "The component urn:qanary:QE-SparqlQueryExecutedAutomaticallyOnWikidataOrDBpedia has added 0 annotation(s) to the graph: "@en , "Die Komponente urn:qanary:QE-SparqlQueryExecutedAutomaticallyOnWikidataOrDBpedia hat 0 Annotation(en) zum Graph hinzugefügt: "@de ."""
-
-g = Graph()
-g.parse(data=string)
